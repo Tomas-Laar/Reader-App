11efdb31fa077f53d49919836fe4e6a9d1bf9219
Tomas-Laar <tomaslaar@MacBook-Pro-van-Tomas.local>
2014-10-16 15:23:50 +0200

    buttons left right functioning!!

--- js/javascript.js
+++ js/javascript.js
@@ -1,26 +1,25 @@
 $( window ).ready(function(){
-console.log($(".columns").width());
-$(".columns").css("height",$(window).height());
-
+    console.log($(".columns").width());
+    $(".columns").css("height",$(window).height()-((40+20+20)+$("#header").height()));
 });
 
 $( window ).resize(function(){
     console.log( $(".columns").width() );
-    $(".columns").css("height",$(window).height()-120);
+    $(".columns").css("height",$(window).height()-((40+20+20)+$("#header").height()));
 });
 
 $('#right').click(function(e) {
 	e.preventDefault();
     scrollamount = $(window).scrollLeft() + $( ".columns" ).width()+100;
-    console.log(scrollamount);
-	console.log('clicked! right! ' + scrollamount);
+    //console.log(scrollamount);
+	//console.log('clicked! right! ' + scrollamount);
     $(window).scrollLeft(parseInt(scrollamount));
 });
 
 $('#left').click(function(e) {
 	e.preventDefault();
-    scrollamount = $(window).scrollRight() + $( ".columns" ).width()-100;
-    console.log(scrollamount);
-	console.log('clicked! left! ' + scrollamount);
-    $(window).scrollRight(parseInt(scrollamount));
+    scrollamount = $(window).scrollLeft() - $( ".columns" ).width()-100;
+    //console.log(scrollamount);
+	//console.log('clicked! left! ' - scrollamount);
+    $(window).scrollLeft(parseInt(scrollamount));
 });
