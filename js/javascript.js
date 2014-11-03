441acaace84dec95e55dd5a1a8b7e19a9a14b6c0
Tomas-Laar <tomaslaar@MacBook-Pro-van-Tomas.local>
2014-11-03 15:22:48 +0100

    nav bar/text layout

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,21 @@
+var gridWidth = function() {
+    return $( ".columns" ).width() + 100;
+};
+
+var snap = function() {
+    // what is the width of half a column
+    var half = gridWidth() / 2;
+    // how many columns have we scrolled by completely
+    var n = parseInt( $(window).scrollLeft() / gridWidth() );
+    // how many pixels into the next column
+    var rest = $(window).scrollLeft() % gridWidth();
+    if (rest > half) {
+        $(window).scrollLeft( (n + 1) *  gridWidth() )
+    } else {
+       $(window).scrollLeft( n *  gridWidth() ) 
+    }
+};
+
 $( window ).ready(function(){
     console.log($(".columns").width());
     $(".columns").css("height",$(window).height()-((40+20+20)+$("#header").height()));
@@ -6,11 +24,18 @@ $( window ).ready(function(){
 $( window ).resize(function(){
     console.log( $(".columns").width() );
     $(".columns").css("height",$(window).height()-((40+20+20)+$("#header").height()));
+    snap();
 });
+
+$(window)
+  .on("scrollstop", function() {
+    snap();
+    // console.log('Scrollstop' + $(window).scrollLeft() % gridWidth());
+  })
 
 $('#right').click(function(e) {
 	e.preventDefault();
-    scrollamount = $(window).scrollLeft() + $( ".columns" ).width()+100;
+    scrollamount = $(window).scrollLeft() + gridWidth();
     //console.log(scrollamount);
 	//console.log('clicked! right! ' + scrollamount);
     $(window).scrollLeft(parseInt(scrollamount));
@@ -18,8 +43,12 @@ $('#right').click(function(e) {
 
 $('#left').click(function(e) {
 	e.preventDefault();
-    scrollamount = $(window).scrollLeft() - $( ".columns" ).width()-100;
+    scrollamount = $(window).scrollLeft() - gridWidth();
     //console.log(scrollamount);
 	//console.log('clicked! left! ' - scrollamount);
     $(window).scrollLeft(parseInt(scrollamount));
 });
+
+
+
+
